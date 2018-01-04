pspiworld/piworld

commit 139f9da532dab8a79471f4bc50c21bad4236917d

Add command line options to control what is shown
Options added with example settings:

  --show-chat-text 1
  --show-crosshairs 1
  --show-clouds 1
  --show-info-text 1
  --show-item 1
  --show-lights 1
  --show-plants 0
  --show-player-names 1
  --show-trees 0
  --show-wireframe 0
  --view 3  # Be careful with higher values, you may run out of memory.
  --vsync 0

src/world.c
@@ -33,7 +33,7 @@ void create_world(int p, int q, world_func func, void *arg) {
                 func(x, y, z, w * flag, arg);
             }
             if (w == 1) {
-                if (SHOW_PLANTS) {
+                if (config->show_plants) {
                     // grass
                     if (simplex2(-x * 0.1, z * 0.1, 4, 0.8, 2) > 0.6) {
                         func(x, h, z, TALL_GRASS * flag, arg);
@@ -45,7 +45,7 @@ void create_world(int p, int q, world_func func, void *arg) {
                     }
                 }
                 // trees
-                int ok = SHOW_TREES;
+                int ok = config->show_trees;
                 if (dx - 4 < 0 || dz - 4 < 0 ||
                     dx + 4 >= CHUNK_SIZE || dz + 4 >= CHUNK_SIZE)
                 {
@@ -71,7 +71,7 @@ void create_world(int p, int q, world_func func, void *arg) {
                 }
             }
             // clouds
-            if (SHOW_CLOUDS) {
+            if (config->show_clouds) {
                 for (int y = 64; y < 72; y++) {
                     if (simplex3(
                         x * 0.01, y * 0.1, z * 0.01, 8, 0.5, 2) > 0.75)

src/config.c
@@ -18,7 +18,19 @@ void reset_config() {
     get_default_db_path(config->db_path);
     config->port = DEFAULT_PORT;
     config->server[0] = '\0';
+    config->show_chat_text = SHOW_CHAT_TEXT;
+    config->show_clouds = SHOW_CLOUDS;
+    config->show_crosshairs = SHOW_CROSSHAIRS;
+    config->show_info_text = SHOW_INFO_TEXT;
+    config->show_item = SHOW_ITEM;
+    config->show_lights = SHOW_LIGHTS;
+    config->show_plants = SHOW_PLANTS;
+    config->show_player_names = SHOW_PLAYER_NAMES;
+    config->show_trees = SHOW_TREES;
+    config->show_wireframe = SHOW_WIREFRAME;
     config->verbose = 0;
+    config->view = get_starting_draw_radius();
+    config->vsync = VSYNC;
     strncpy(config->window_title, WINDOW_TITLE, strlen(WINDOW_TITLE));
     config->window_x = CENTRE_IN_SCREEN;
     config->window_y = CENTRE_IN_SCREEN;
@@ -48,6 +60,30 @@ void get_server_db_cache_path(char *path)
         "%s/cache.%s.%d.db", config->path, config->server, config->port);
 }
 
+/*
+ * Return a draw radius that will fit into the current size of GPU RAM.
+ */
+int get_starting_draw_radius()
+{
+    int gpu_mb = pg_get_gpu_mem_size();
+    if (gpu_mb < 48) {
+        // A draw distance of 1 is not enough for the game to be usable, but
+        // this does at least show something on screen (for low resolutions
+        // only - higher ones will crash the game with low GPU RAM).
+        return 1;
+    } else if (gpu_mb < 64) {
+        return 2;
+    } else if (gpu_mb < 128) {
+        // A GPU RAM size of 64M will result in rendering issues for draw
+        // distances greater than 3 (with a chunk size of 16).
+        return 3;
+    } else {
+        // For the Raspberry Pi reduce amount to draw to both fit into 128MiB
+        // of GPU RAM and keep the render speed at a reasonable smoothness.
+        return 5;
+    }
+}
+
 void parse_startup_config(int argc, char **argv) {
     int c;
     const char *opt_name;
@@ -55,13 +91,25 @@ void parse_startup_config(int argc, char **argv) {
     while (1) {
         int option_index = 0;
         static struct option long_options[] = {
-            {"port",         required_argument, 0,  0 },
-            {"server",       required_argument, 0,  0 },
-            {"verbose",      no_argument,       0,  0 },
-            {"window-size",  required_argument, 0,  0 },
-            {"window-title", required_argument, 0,  0 },
-            {"window-xy",    required_argument, 0,  0 },
-            {0,              0,                 0,  0 }
+            {"port",              required_argument, 0,  0 },
+            {"server",            required_argument, 0,  0 },
+            {"show-chat-text",    required_argument, 0,  0 },
+            {"show-crosshairs",   required_argument, 0,  0 },
+            {"show-clouds",       required_argument, 0,  0 },
+            {"show-info-text",    required_argument, 0,  0 },
+            {"show-item",         required_argument, 0,  0 },
+            {"show-lights",       required_argument, 0,  0 },
+            {"show-plants",       required_argument, 0,  0 },
+            {"show-player-names", required_argument, 0,  0 },
+            {"show-trees",        required_argument, 0,  0 },
+            {"show-wireframe",    required_argument, 0,  0 },
+            {"verbose",           no_argument,       0,  0 },
+            {"view",              required_argument, 0,  0 },
+            {"vsync",             required_argument, 0,  0 },
+            {"window-size",       required_argument, 0,  0 },
+            {"window-title",      required_argument, 0,  0 },
+            {"window-xy",         required_argument, 0,  0 },
+            {0,                   0,                 0,  0 }
         };
 
         c = getopt_long(argc, argv, "", long_options, &option_index);
@@ -73,8 +121,32 @@ void parse_startup_config(int argc, char **argv) {
             opt_name = long_options[option_index].name;
             if (strncmp(opt_name, "port", 4) == 0 &&
                 sscanf(optarg, "%d", &config->port) == 1) {
+            } else if (strncmp(opt_name, "show-chat-text", 15) == 0 &&
+                       sscanf(optarg, "%d", &config->show_chat_text) == 1) {
+            } else if (strncmp(opt_name, "show-crosshairs", 15) == 0 &&
+                       sscanf(optarg, "%d", &config->show_crosshairs) == 1) {
+            } else if (strncmp(opt_name, "show-clouds", 11) == 0 &&
+                       sscanf(optarg, "%d", &config->show_clouds) == 1) {
+            } else if (strncmp(opt_name, "show-info-text", 14) == 0 &&
+                       sscanf(optarg, "%d", &config->show_info_text) == 1) {
+            } else if (strncmp(opt_name, "show-item", 9) == 0 &&
+                       sscanf(optarg, "%d", &config->show_item) == 1) {
+            } else if (strncmp(opt_name, "show-lights", 11) == 0 &&
+                       sscanf(optarg, "%d", &config->show_lights) == 1) {
+            } else if (strncmp(opt_name, "show-plants", 11) == 0 &&
+                       sscanf(optarg, "%d", &config->show_plants) == 1) {
+            } else if (strncmp(opt_name, "show-player-names", 17) == 0 &&
+                       sscanf(optarg, "%d", &config->show_player_names) == 1) {
+            } else if (strncmp(opt_name, "show-trees", 10) == 0 &&
+                       sscanf(optarg, "%d", &config->show_trees) == 1) {
+            } else if (strncmp(opt_name, "show-wireframe", 14) == 0 &&
+                       sscanf(optarg, "%d", &config->show_wireframe) == 1) {
             } else if (strncmp(opt_name, "verbose", 7) == 0) {
                 config->verbose = 1;
+            } else if (strncmp(opt_name, "view", 4) == 0 &&
+                       sscanf(optarg, "%d", &config->view) == 1) {
+            } else if (strncmp(opt_name, "vsync", 5) == 0 &&
+                       sscanf(optarg, "%d", &config->vsync) == 1) {
             } else if (strncmp(opt_name, "window-size", 11) == 0 &&
                        sscanf(optarg, "%d,%d", &config->window_width,
                               &config->window_height) == 2) {

src/config.h
@@ -53,8 +53,20 @@ typedef struct {
     char path[MAX_PATH_LENGTH];
     char db_path[MAX_PATH_LENGTH];
     int port;
+    int show_chat_text;
+    int show_clouds;
+    int show_crosshairs;
+    int show_info_text;
+    int show_item;
+    int show_lights;
+    int show_plants;
+    int show_player_names;
+    int show_trees;
+    int show_wireframe;
     char server[MAX_ADDR_LENGTH];
     int verbose;
+    int view;
+    int vsync;
     char window_title[MAX_TITLE_LENGTH];
     int window_x;
     int window_y;
@@ -68,5 +80,6 @@ void reset_config();
 void get_config_path(char *path);
 void get_default_db_path(char *path);
 void get_server_db_cache_path(char *path);
+int get_starting_draw_radius();
 void parse_startup_config(int argc, char **argv);
 

src/main.c
@@ -175,30 +175,6 @@ typedef struct {
 static Model model;
 static Model *g = &model;
 
-/*
- * Return a draw radius that will fit into the current size of GPU RAM.
- */
-int get_starting_draw_radius()
-{
-    int gpu_mb = pg_get_gpu_mem_size();
-    if (gpu_mb < 48) {
-        // A draw distance of 1 is not enough for the game to be usable, but
-        // this does at least show something on screen (for low resolutions
-        // only - higher ones will crash the game with low GPU RAM).
-        return 1;
-    } else if (gpu_mb < 64) {
-        return 2;
-    } else if (gpu_mb < 128) {
-        // A GPU RAM size of 64M will result in rendering issues for draw
-        // distances greater than 3 (with a chunk size of 16).
-        return 3;
-    } else {
-        // For the Raspberry Pi reduce amount to draw to both fit into 128MiB
-        // of GPU RAM and keep the render speed at a reasonable smoothness.
-        return 5;
-    }
-}
-
 int chunked(float x) {
     return floorf(roundf(x) / CHUNK_SIZE);
 }
@@ -877,7 +853,7 @@ void gen_sign_buffer(Chunk *chunk) {
 }
 
 int has_lights(Chunk *chunk) {
-    if (!SHOW_LIGHTS) {
+    if (!config->show_lights) {
         return 0;
     }
     for (int dp = -1; dp <= 1; dp++) {
@@ -1002,7 +978,7 @@ void compute_chunk(WorkerItem *item) {
 
     // check for lights
     int has_light = 0;
-    if (SHOW_LIGHTS) {
+    if (config->show_lights) {
         for (int a = 0; a < 3; a++) {
             for (int b = 0; b < 3; b++) {
                 Map *map = item->light_maps[a][b];
@@ -2686,7 +2662,7 @@ int main(int argc, char **argv) {
     g->height = config->window_height;
     pg_start(config->window_title, config->window_x, config->window_y,
              g->width, g->height);
-    pg_swap_interval(VSYNC);
+    pg_swap_interval(config->vsync);
     set_key_press_handler(*handle_key_press);
     set_key_release_handler(*handle_key_release);
     set_mouse_release_handler(*handle_mouse_release);
@@ -2792,7 +2768,7 @@ int main(int argc, char **argv) {
         snprintf(g->db_path, MAX_PATH_LENGTH, "%s", config->db_path);
     }
 
-    int draw_radius = get_starting_draw_radius();
+    int draw_radius = config->view;
     g->create_radius = draw_radius;
     g->render_radius = draw_radius;
     g->delete_radius = draw_radius + 4;
@@ -2926,16 +2902,16 @@ int main(int argc, char **argv) {
             render_signs(&text_attrib, player);
             render_sign(&text_attrib, player);
             render_players(&block_attrib, player);
-            if (SHOW_WIREFRAME) {
+            if (config->show_wireframe) {
                 render_wireframe(&line_attrib, player);
             }
 
             // RENDER HUD //
             glClear(GL_DEPTH_BUFFER_BIT);
-            if (SHOW_CROSSHAIRS) {
+            if (config->show_crosshairs) {
                 render_crosshairs(&line_attrib);
             }
-            if (SHOW_ITEM) {
+            if (config->show_item) {
                 render_item(&block_attrib);
             }
 
@@ -2944,7 +2920,7 @@ int main(int argc, char **argv) {
             float ts = 12 * g->scale;
             float tx = ts / 2;
             float ty = g->height - ts;
-            if (SHOW_INFO_TEXT) {
+            if (config->show_info_text) {
                 int hour = time_of_day() * 24;
                 char am_pm = hour < 12 ? 'a' : 'p';
                 hour = hour % 12;
@@ -2981,7 +2957,7 @@ int main(int argc, char **argv) {
                     ty -= ts * 2;
                 }
             }
-            if (SHOW_CHAT_TEXT) {
+            if (config->show_chat_text) {
                 for (int i = 0; i < MAX_MESSAGES; i++) {
                     int index = (g->message_index + i) % MAX_MESSAGES;
                     if (strlen(g->messages[index])) {
@@ -2998,7 +2974,7 @@ int main(int argc, char **argv) {
                 render_text_cursor(&line_attrib, tx + ts * (g->text_cursor+1) + ts/2, ty);
                 ty -= ts * 2;
             }
-            if (SHOW_PLAYER_NAMES) {
+            if (config->show_player_names) {
                 if (player != me) {
                     render_text(&text_attrib, ALIGN_CENTER,
                         g->width / 2, ts, ts, player->name);
@@ -3040,7 +3016,7 @@ int main(int argc, char **argv) {
                 render_signs(&text_attrib, player);
                 render_players(&block_attrib, player);
                 glClear(GL_DEPTH_BUFFER_BIT);
-                if (SHOW_PLAYER_NAMES) {
+                if (config->show_player_names) {
                     render_text(&text_attrib, ALIGN_CENTER,
                         pw / 2, ts, ts, player->name);
                 }

commit c81b5dddef5d627821aaf78cd2be6ca3bf08e2be

Fix server crash
The server build of src/world.c does not use the dynamic config
introduced in commit 139f9da5.

src/world.c
@@ -33,7 +33,11 @@ void create_world(int p, int q, world_func func, void *arg) {
                 func(x, y, z, w * flag, arg);
             }
             if (w == 1) {
+#ifdef SERVER
+                if (SHOW_PLANTS) {
+#else
                 if (config->show_plants) {
+#endif
                     // grass
                     if (simplex2(-x * 0.1, z * 0.1, 4, 0.8, 2) > 0.6) {
                         func(x, h, z, TALL_GRASS * flag, arg);
@@ -45,7 +49,11 @@ void create_world(int p, int q, world_func func, void *arg) {
                     }
                 }
                 // trees
+#ifdef SERVER
+                int ok = SHOW_TREES;
+#else
                 int ok = config->show_trees;
+#endif
                 if (dx - 4 < 0 || dz - 4 < 0 ||
                     dx + 4 >= CHUNK_SIZE || dz + 4 >= CHUNK_SIZE)
                 {
@@ -71,7 +79,11 @@ void create_world(int p, int q, world_func func, void *arg) {
                 }
             }
             // clouds
-            if (config->show_clouds) {
+#ifdef SERVER
+                if (SHOW_CLOUDS) {
+#else
+                if (config->show_clouds) {
+#endif
                 for (int y = 64; y < 72; y++) {
                     if (simplex3(
                         x * 0.01, y * 0.1, z * 0.01, 8, 0.5, 2) > 0.75)

world.py
@@ -1,4 +1,4 @@
-# gcc -std=c99 -O3 -shared -fpic -o world \
+# gcc -DSERVER -std=c99 -O3 -shared -fpic -o world \
 #   -I src -I deps/noise deps/noise/noise.c src/world.c
 
 from ctypes import CDLL, CFUNCTYPE, c_float, c_int, c_void_p